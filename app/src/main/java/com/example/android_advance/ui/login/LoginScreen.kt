e30b4b8ff50edd0b8c640d1530861063402d6746
tuyenkieuminh <87085555+tuyenkieuminh@users.noreply.github.com>
2024-02-04 11:34:10 +0700

    add search and call history ui

--- app/src/main/java/com/example/android_advance/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/android_advance/ui/login/LoginScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.android_advance.navigation.Route
