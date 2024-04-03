f13f5a6a6b4a2866d7cfdf7c4b46d39d8ff8950e
NgKhacDuy <2002nguyenkhacduy@gmail.com>
2024-04-03 14:44:34 +0700

    changes: change url socket

--- app/src/main/java/com/example/android_advance/socketio/SocketIo.kt
+++ app/src/main/java/com/example/android_advance/socketio/SocketIo.kt
@@ -19,8 +19,8 @@ class SocketManager @Inject constructor(@ApplicationContext private val context:
     val appSharedPreference = AppSharedPreference(context)
     val appInterceptor = appInterceptor(context)
 
-    //    private val url = "https://android-nang-cao-backend.onrender.com"
-    private val url = "http://192.168.100.2:3000"
+    private val url = "https://android-nang-cao-backend.onrender.com"
+//    private val url = "http://192.168.100.2:3000"
 
     private var options = IO.Options().apply {
         extraHeaders = mapOf("Authorization" to listOf(appSharedPreference.accessToken))
