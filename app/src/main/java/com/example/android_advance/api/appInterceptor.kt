13dcc2a9cc21b00765e94c5edea5eb9547d878ee
NgKhacDuy <2002nguyenkhacduy@gmail.com>
2024-04-08 07:42:45 +0700

    feat: render expanded img in chat

--- app/src/main/java/com/example/android_advance/api/appInterceptor.kt
+++ app/src/main/java/com/example/android_advance/api/appInterceptor.kt
@@ -2,12 +2,21 @@ package com.example.android_advance.api
 
 import android.content.Context
 import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.android_advance.database.DatabaseHelper
 import com.example.android_advance.model.request.RefreshRequest
 import com.example.android_advance.model.response.SigninResponse
 import com.example.android_advance.shared_preference.AppSharedPreference
+import com.google.gson.Gson
 import dagger.hilt.android.qualifiers.ApplicationContext
 import okhttp3.Interceptor
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import java.io.IOException
@@ -16,52 +25,57 @@ import javax.inject.Inject
 class appInterceptor @Inject constructor(
     @ApplicationContext private val context: Context,
 ) : Interceptor {
+    private lateinit var db: DatabaseHelper
+    val appSharedPreference = AppSharedPreference(context)
+
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        var request = chain.request()
-        val originResponse = chain.proceed(request)
-        var tryCount = 0
-        while (!originResponse.isSuccessful && tryCount <= 3) {
-            tryCount++
-            val newToken = refreshTokenFunction()
-            if (newToken != "") {
-                request =
-                    request.newBuilder().header("Authorization", "Bearer $newToken").build()
+        var originRequest = chain.request()
+        var originResponse = chain.proceed(originRequest)
+        val refreshRequest = RefreshRequest(refreshToken = appSharedPreference.refreshToken)
+        if (originResponse.code == 401) {
+            val body =
+                Gson().toJson(refreshRequest).toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+            val authRequest = chain.request().newBuilder()
+                .removeHeader("Authorization")
+                .url(apiConstant.baseApi + apiConstant.userRefresh)
+                .post(body)
+                .build()
+            val tokenResponse = chain.proceed(authRequest)
+            if (tokenResponse.code == 200) {
                 originResponse.close()
-                return chain.proceed(request)
+                var json = JSONObject(tokenResponse.body?.string()!!)
+                val data = JSONObject(json.getString("data"))
+                val accessToken = data.getString("accessToken")
+                appSharedPreference.accessToken = accessToken
+                val temp =
+                    chain.proceed(
+                        originRequest.newBuilder()
+                            .removeHeader("Authorization")
+                            .addHeader("Authorization", "Bearer $accessToken").build()
+                    )
+                originResponse = temp
+            } else {
+                deleteToken()
+                deleteSqlite()
+                val navController = NavController(context)
+                navController.navigate(route = "auth") {
+                    popUpTo("home") {
+                        inclusive = true
+                    }
+                }
             }
         }
         return originResponse
     }
 
-    fun refreshTokenFunction(): String {
-        val apiClient: APIClient = APIClient(context)
-        val appSharedPreference = AppSharedPreference(context)
-        val refreshRequest = RefreshRequest(refreshToken = appSharedPreference.refreshToken)
-        val apiService = apiClient.client()?.create(ApiInterface::class.java)
-        val call = apiService?.refresh(refreshRequest)
-        call?.enqueue(object : Callback<ApiResponse.BaseApiResponse<SigninResponse>> {
-            override fun onResponse(
-                call: Call<ApiResponse.BaseApiResponse<SigninResponse>>,
-                response: retrofit2.Response<ApiResponse.BaseApiResponse<SigninResponse>>
-            ) {
-                if (response.isSuccessful) {
-                    Log.e("REFRESH", "REFRESH SUCCESSFULLY")
-                    appSharedPreference.accessToken = response.body()?.data?.accessToken.toString()
-                } else {
-                    Log.e("REFRESH", "REFRESH FAILED")
-//                    TODO: add logout function
-                }
-            }
+    fun deleteToken() {
+        appSharedPreference.refreshToken = ""
+        appSharedPreference.accessToken = ""
+    }
 
-            override fun onFailure(
-                call: Call<ApiResponse.BaseApiResponse<SigninResponse>>,
-                t: Throwable
-            ) {
-                Log.e("Signup", "Error: $t")
-            }
-
-        })
-        return appSharedPreference.refreshToken
+    fun deleteSqlite() {
+        db = DatabaseHelper(context)
+        db.deleteUser()
     }
 }
