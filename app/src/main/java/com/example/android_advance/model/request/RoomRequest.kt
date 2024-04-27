df0c002fc3e41564b1a0437c37ee8ec7a82b96d0
NgKhacDuy <90504360+NgKhacDuy@users.noreply.github.com>
2024-04-27 21:07:02 +0700

    hotfix: add owner id at first (#75)

--- app/src/main/java/com/example/android_advance/model/request/RoomRequest.kt
+++ app/src/main/java/com/example/android_advance/model/request/RoomRequest.kt
@@ -4,12 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 class RoomRequest {
     @SerializedName("listUser")
-    var listUser: ArrayList<String>
+    var listUser: MutableList<String>
 
     @SerializedName("name")
     var groupName: String
 
-    constructor(listUser: ArrayList<String>, groupName: String) {
+    constructor(listUser: MutableList<String>, groupName: String) {
         this.listUser = listUser
         this.groupName = groupName
     }
