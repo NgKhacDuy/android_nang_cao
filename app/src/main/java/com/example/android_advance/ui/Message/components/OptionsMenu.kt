5445c598d461d4b4a335e87d95764dd2463f95c5
NgKhacDuy <90504360+NgKhacDuy@users.noreply.github.com>
2024-05-05 12:20:46 +0700

    Lamduy add menu partner avatar (#78)
    * feat: adding partner avatar to option menu, adjutt homescrren display self avatar
    
    * feat: adding partner nickname
    
    ---------
    
    Co-authored-by: DuyHuynh153 <huynhlamduylcd@gmail.com>

--- app/src/main/java/com/example/android_advance/ui/Message/components/OptionsMenu.kt
+++ app/src/main/java/com/example/android_advance/ui/Message/components/OptionsMenu.kt
@@ -1,27 +1,18 @@
 
 import android.util.Log
-import android.util.Size
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -33,10 +24,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Block
 import androidx.compose.material.icons.filled.Group
-import androidx.compose.material.icons.filled.IosShare
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.PersonAdd
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
@@ -50,12 +38,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -74,8 +60,7 @@ import com.example.android_advance.ui.Screen.SettingItem
 //@Preview
 @Composable
 //fun OptionsMenu() {
-fun OptionsMenu(navController: NavController, idRoom: String, partnerName: String) {
-//    var partnerName by remember { mutableStateOf("duyhuynhlam") }
+fun OptionsMenu(navController: NavController, idRoom: String, partnerName: String, partnerNickname: String? = null) {
     var searchDialogShown by remember { mutableStateOf(false) }
     var returnKeyword by remember { mutableStateOf("") }
     var request_key by remember { mutableStateOf("") }
@@ -92,6 +77,8 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
     // for handing click the partner image
     var isImageDialogShown by remember { mutableStateOf(false) }
 
+    // nicknames
+    var tempNickname by remember { mutableStateOf("") }
 
     messageState.value?.let { messages ->
 
@@ -105,6 +92,9 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
         }
 
 
+    }
+    if (!partnerNickname.isNullOrBlank()){
+        Log.e("partnerNickname", partnerNickname)
     }
 
 
@@ -164,28 +154,11 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
                                 ,
                             alignment = Alignment.Center
                         )
-                        partnerName?.let { Text(text = it) }
+                        partnerName?.let {Text(text = it + if (tempNickname.isNotEmpty()) " ($tempNickname)" else if (!partnerNickname.isNullOrEmpty()) " ($partnerNickname)" else "")
+                        }
 
                     }
-//                AsyncImage(model = url,
-//                    contentDescription = "partner avatar",
-//                    modifier = Modifier
-//                        .size(100.dp)
-//                        .clip(CircleShape)
-//                        .clickable {
-//                            isImageDialogShown = true
-//                        }
-//                        ,
-//                    alignment = Alignment.Center
-//                )
-//                    if (!partnerName.isNullOrEmpty()){
-//                        Text(text = roomDto.partner!!.name!!)
 //
-//                    }
-//                    else {
-//                        Text("room chua cos ten")
-//
-//                    }
 
             };
                 content.let {
@@ -194,7 +167,7 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
                     if (!viewModel.roomDto?.isGroup!!) {
                         SettingItem(
                             icon = Icons.Default.PersonAdd,
-                            title = "NickName:",
+                            title = "NickName:  ${if (tempNickname.isNotEmpty()) " ($tempNickname)" else if (!partnerNickname.isNullOrEmpty()) " ($partnerNickname)" else ""}"  ,
                             onClick = {
                                 searchDialogShown = true
                                 request_key = "nickname"
@@ -213,7 +186,7 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
                     if (!viewModel.roomDto.isGroup!!) {
                         SettingItem(
                             icon = Icons.Default.Group,
-                            title = "Create Group with $partnerName ${if (returnKeyword.isNotEmpty()) " ($returnKeyword)" else ""}",
+                            title = "Create Group with $partnerName ",
                             onClick = {
                                 navController.navigate(Route.CreateGroupScreen.route)
                             }
@@ -229,15 +202,6 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
                             }
                         )
                     }
-                    if (!viewModel.roomDto.isGroup!!) {
-                        SettingItem(
-                            icon = Icons.Default.Block,
-                            title = "Block $partnerName ${if (returnKeyword.isNotEmpty()) " ($returnKeyword)" else ""}",
-                            onClick = {
-                            }
-                        )
-                    }
-//                    DisplayImages()
                     messageState.value?.let { messages ->
                         DisplayImages(messages)
                     }
@@ -264,11 +228,11 @@ fun OptionsMenu(navController: NavController, idRoom: String, partnerName: Strin
                     searchDialogShown = false
                 },
                 onSearch = { keyword ->
-                    returnKeyword = keyword
-//                val previousBackEntry = navController.previousBackStackEntry
-//                previousBackEntry?.savedStateHandle?.set("key_words_return", keyword)
-//                navController.popBackStack()
-                    Log.e("nickname key", returnKeyword)
+                    tempNickname = keyword
+                    val previousBackEntry = navController.previousBackStackEntry
+                    previousBackEntry?.savedStateHandle?.set("nickname_return", tempNickname)
+//                    navController.popBackStack()
+                    Log.e("nickname key", tempNickname)
                     searchDialogShown = false
                 },
                 request = "Nickname"
